212549589b54f1fe614c264b42e67ad10d4973d3
timmy-edibo <timothyedibo@gmail.com>
2022-05-18 06:50:38 +0100

    world of numbers

---
+++ numbers.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+
+
+a=40
+b=4
+
+# Addition
+echo "Addition of $a+$b = "$[a+b]
+
+#Multiplication
+echo "Multiplication of $a*$b = " $[a*b]
+
+#Subtraction
+echo "Subtraction of $a-$b =" $[a-b]
+
+
+#Division
+echo "Division of $a/$b = " $[a/b]
+
+#Modulus or reminder
+echo "reminder of $a%$b = " $[a%b]
+
