8f5fa9062a69718becb2f63f25764c2a9f9ee67f
timmy-edibo <timothyedibo@gmail.com>
2022-05-24 23:48:20 +0100

    Cut operation in bash scripts

---
+++ cut_advance.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#https://github.com/Timmy-Edibo/bash-scripts
+
+file=/home/timmy/projects/bash-scripts/cut.txt
+
+while read line || [ -n "$line" ]; 
+do 
+    echo "$line"; 
+
+done < $file | cut -d ' ' -f 3
+
+echo "Linux cut command is useful" | cut  -d ' ' -f 1,2,3
---
+++ cut_beginners.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+#https://github.com/Timmy-Edibo/bash-scripts
+
+file=/home/timmy/projects/bash-scripts/cut.txt
+
+
+
+# To check 2nd and 7th Character
+while read line || [ -n "$line" ]; 
+do 
+    echo "$line"; 
+
+done < $file | cut -c 2,7
+
+
+# To check till the 4th charcter
+
+while read line || [ -n "$line" ]; 
+do 
+    echo "$line"; 
+
+done < $file | cut -c 1-4
+
+
